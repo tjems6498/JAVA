86ea2c2a12bd8cca5d2b1e6adf84759bc1305ce2
tjems6498 <tjems6498@gmail.com>
2021-03-03 20:46:46 +0900

    commit

---
+++ helloWeb/src/memController/SignUp.java
@@ -0,0 +1,62 @@
+package memController;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import mem.Member;
+import mem.Service;
+import mem.ServiceImpl;
+
+/**
+ * Servlet implementation class SignUp
+ */
+@WebServlet("/SignUp")
+public class SignUp extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SignUp() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String result = "form.jsp";
+		Service service = new ServiceImpl();
+		String id = request.getParameter("id");
+		String pwd = request.getParameter("pwd");
+		String name = request.getParameter("name");
+		String email = request.getParameter("email");
+	
+		Member m = new Member(id, pwd, name, email);
+		boolean flag = service.addMember(m);
+		System.out.println(flag);
+		if (flag) {
+			result = "cssTest.jsp";
+		}
+		RequestDispatcher ds = request.getRequestDispatcher(result);
+		ds.forward(request, response);		
+		
+	}
+
+}
