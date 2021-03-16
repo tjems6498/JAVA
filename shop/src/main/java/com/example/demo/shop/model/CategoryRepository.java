86f72e50cf5f95130d5b23970ed45c6d432e07fd
tjems6498 <53398821+tjems6498@users.noreply.github.com>
2021-03-16 16:13:54 +0900

    Delete shop directory

--- shop/src/main/java/com/example/demo/shop/model/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.shop.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-
-}
--- shop/src/main/java/com/example/demo/shop/model/ShopOrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.shop.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ShopOrderRepository extends JpaRepository<ShopOrder, Integer> {
-
-}
